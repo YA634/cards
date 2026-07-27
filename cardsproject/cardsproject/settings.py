5a10793cd61cfad350525d93db273d9258553bba
Akihiro Yanagisawa <aki634333year@gmail.com>
2026-07-27 16:33:13 +0900

    fix requirements.txt

--- cardsproject/cardsproject/settings.py
+++ cardsproject/cardsproject/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p+=!r@l&j+oe8lv(1a9ac@p7kzzrio@&xq_8@b1r+cqvhddp$0'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
