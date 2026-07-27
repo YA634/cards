a061f5011c4685722ac92ca59765566cd211ba1e
Akihiro Yanagisawa <aki634333year@gmail.com>
2026-07-27 16:26:55 +0900

    change DB 2026/7/27/16:26

--- cardsproject/accounts/models.py
+++ cardsproject/accounts/models.py
@@ -1,3 +1,4 @@
+# accounts
 from django.db import models
 
 # Create your models here.
--- cardsproject/baccara/models.py
+++ cardsproject/baccara/models.py
@@ -1,3 +1,4 @@
+# baccara
 from django.db import models
 from django.conf import settings
 # Create your models here.
